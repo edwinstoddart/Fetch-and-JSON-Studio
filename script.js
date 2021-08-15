eb5dd4c23c012ef30fca0eeeb740c8b49eb5a60b
Edwin DeJesus <edwinstoddart.student@gmail.com>
2021-08-15 07:58:06 -0400

    Completed assignment and bonus assignments.

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-// TODO: add code here
+window.addEventListener('load', () => {
+    fetch('https://handlers.education.launchcode.org/static/astronauts.json').then((response) => {
+        response.json().then((json) => {
+            json.sort((a, b) => {
+                let keyA = a.hoursInSpace;
+                let keyB = b.hoursInSpace;
+                if (keyA < keyB) {return -1};
+                if (keyA > keyB) {return 1};
+                return 0;
+            });
+
+            let htmlBody = '';
+            let color = '';
+            for (let i = 0; i < json.length; i++) {
+                json[i].active ? textColor = 'green' : textColor = 'black';
+                htmlBody += `
+                <div class='astronaut'>
+                    <div class='bio'>
+                        <h3>${json[i].firstName} ${json[i].lastName}</h3>
+                        <ul>
+                            <li>Hours In Space: ${json[i].hoursInSpace}</li>
+                            <li><font color='${textColor}'>Active: ${json[i].active}</font></li>
+                            <li>Skills: ${json[i].skills}</li>
+                        </ul>
+                    </div>
+                    <img class='avatar' src='${json[i].picture}'>
+                </div>
+                `;
+            }
+            // Update HTML here
+            const container = document.getElementById('container');
+            container.innerHTML = htmlBody;
+
+            const astronautCount = document.getElementById('mainHeader');
+            astronautCount.innerHTML += `: ${json.length}`;
+        });
+    });
+});
